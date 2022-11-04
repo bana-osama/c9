84e94dac8481cd19ca40eca6edb94d15214aa7cd
bana-osama <117452701+bana-osama@users.noreply.github.com>
2022-11-04 16:26:50 +0400

    Add files via upload
    my first vsc files

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var ball ;
+
+
+function setup() {
+  createCanvas(400,400);
+ball=createSprite(200,200,30,50)
+
+}
+
+function draw() 
+{
+  background(30);
+if(keyDown("RIGHT_ARROW")){
+  ball.position.x += 5
+}
+
+
+
+drawSprites();
+}
+
+
+
+
