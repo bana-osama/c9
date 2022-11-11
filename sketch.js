00812cbbe4c1244fdcc0081e242c4a9e039e4353
bana-osama <117452701+bana-osama@users.noreply.github.com>
2022-11-11 19:45:39 +0400

    Add files via upload
    first project on vs

--- sketch.js
+++ sketch.js
@@ -1,24 +1,40 @@
-var ball ;
-
 
 function setup() {
   createCanvas(400,400);
-ball=createSprite(200,200,30,50)
+  background(51);
+  box = createSprite(200,200,30,30);
 
 }
 
 function draw() 
 {
-  background(30);
-if(keyDown("RIGHT_ARROW")){
-  ball.position.x += 5
+
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
+  if(keyIsDown(RIGHT_ARROW)){
+    background("red")
+  }
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
+  drawSprites();
 }
-
-
-
-drawSprites();
-}
-
-
-
 
